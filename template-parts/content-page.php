9a8066e9f31e28848b34d93ff5df6e844f5b7554
Mapk <uraine@gmail.com>
2018-08-22 09:55:42 -0700

    Optimize the code for the Edit link on pages.

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -25,26 +25,13 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-				edit_post_link(
-					sprintf(
-						wp_kses(
-							/* translators: %s: Name of current post. Only visible to screen readers */
-							__( 'Edit <span class="screen-reader-text">%s</span>', 'mono' ),
-							array(
-								'span' => array(
-									'class' => array(),
-								),
-							)
-						),
-						get_the_title()
-					),
-					'<span class="edit-link">',
-					'</span>'
-				);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
+	<?php edit_post_link(
+        sprintf(
+            /* translators: %s: Name of current post */
+            esc_html__( 'Edit %s', '_s' ),
+            the_title( '<span class="screen-reader-text">"', '"</span>', false )
+        ),
+        '<footer class="entry-footer"><span class="edit-link">',
+        '</span></footer><!-- .entry-footer -->'
+    ); ?>
 </article><!-- #post-<?php the_ID(); ?> -->
