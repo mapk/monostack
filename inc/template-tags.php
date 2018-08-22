fba4069da3de8d59752b970d8786d9913f2337c6
Mapk <uraine@gmail.com>
2018-08-21 21:15:07 -0700

    Styled the post times, removed anchor tags.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -27,7 +27,7 @@ if ( ! function_exists( 'mono_posted_on' ) ) :
 		$posted_on = sprintf(
 			/* translators: %s: post date. */
 			esc_html_x( '%s', 'post date', 'mono' ),
-			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+			$time_string
 		);
 
 		$byline = sprintf(
@@ -36,7 +36,7 @@ if ( ! function_exists( 'mono_posted_on' ) ) :
 			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 		);
 
-		echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+		echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
 
 	}
 endif;
