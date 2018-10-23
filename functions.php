b6e32e85aab32a8b0e98c4ef4450eb0fb632ade0
Mapk <uraine@gmail.com>
2018-10-23 16:19:21 -0700

    Fixed Customizer default background color

--- functions.php
+++ functions.php
@@ -61,7 +61,7 @@ if ( ! function_exists( 'monostack_setup' ) ) :
 
 		// Set up the WordPress core custom background feature.
 		add_theme_support( 'custom-background', apply_filters( '_s_custom_background_args', array(
-			'default-color' => 'ffffff',
+			'default-color' => '282A36',
 			'default-image' => '',
 		) ) );
 
