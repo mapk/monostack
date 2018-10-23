854a5ff7b5263e9a33d2a4614dcc34070e7d588c
Mapk <uraine@gmail.com>
2018-10-23 16:10:52 -0700

    Removed custom header from functions.php

--- functions.php
+++ functions.php
@@ -217,11 +217,6 @@ function monostack_scripts() {
 add_action( 'wp_enqueue_scripts', 'monostack_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
